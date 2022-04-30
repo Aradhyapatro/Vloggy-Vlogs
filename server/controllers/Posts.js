cc6466ae6a10d3a9220d09913a7a4affd5595a21
Aradhya Patro <aradhyapatro14@gmail.com>
2022-04-30 19:06:04 +0530

    Initial setup done

--- server/controllers/Posts.js
+++ server/controllers/Posts.js
@@ -1,25 +1,10 @@
-import PostCard from "../models/Post.js";
+import Post from '../models/Post.js';
 
-export const getPosts = async (req, res) => {
+export const getPosts=async(req,res)={
   try {
-    const postInfo = await PostCard.find();
-    res.status(200).json(postInfo);
+    const data=Post.find();
+    res.status(200).json({info:data});
   } catch (error) {
-    res
-      .status(404)
-      .json({ msg: "data not found check the database connection" });
+    res.status(404).json({msg:"Not found the connection link for mongoatlas"});
   }
-};
-
-export const makePosts = async (req, res) => {
-  const post = body.req;
-  try {
-    const post_new = new PostCard(posts);
-    await post_new.save();
-
-    res.status(200).json({ posting: post_new });
-  } catch (error) {
-    console.log(`Error = ${error}`);
-    res.status(409).json({ Error: error });
-  }
-};
+}
